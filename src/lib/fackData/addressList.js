f522f99ab04b76dbdcbd30c9914112ebe2a02aa7
vipulharsh <harshvipul.1704@gmail.com>
2024-04-12 02:23:48 +0530

    updated address

--- src/lib/fackData/addressList.js
+++ src/lib/fackData/addressList.js
@@ -1,30 +1,30 @@
 export const addressList = [
     {
         id:"01",
-        country:"Address USA:",
-        company:"Architronix Inc.",
-        address:"208 English Road, High Point, NC 27262, USA",
+        country:"Address Bhiwadi:",
+        company:"Jangra Interior",
+        address:"Jangra Complex, opposite Central Market, Bhiwadi, Rajasthan 301019",
         phone:"336 885 6670  ",
-        email:"hello@architronix.com",
-        lat_lng:[35.9588148, -80.0130777]
+        email:"hello@jangra.com",
+        lat_lng:[28.210635, 76.836650]
     },
-    {
-        id:"02",
-        country:"Address Spain:",
-        company:"Architronix",
-        address:"Avda. Valencia, 3, 46891, Palomar (Valencia), SPAIN ",
-        phone:"336 885 6670  ",
-        email:"hello@architronix.com",
-        lat_lng:[38.8545732, -0.5016711]
-    },
-    {
-        id:"03",
-        country:"Address London:",
-        company:"Architronix LTD. ",
-        address:"Avda. Valencia, 3, 46891, London, England",
-        phone:"336 885 6670  ",
-        email:"hello@architronix.com",
-        lat_lng:[51.5132388, -0.0080891]
-    },
+    // {
+    //     id:"02",
+    //     country:"Address Spain:",
+    //     company:"Architronix",
+    //     address:"Avda. Valencia, 3, 46891, Palomar (Valencia), SPAIN ",
+    //     phone:"336 885 6670  ",
+    //     email:"hello@architronix.com",
+    //     lat_lng:[38.8545732, -0.5016711]
+    // },
+    // {
+    //     id:"03",
+    //     country:"Address London:",
+    //     company:"Architronix LTD. ",
+    //     address:"Avda. Valencia, 3, 46891, London, England",
+    //     phone:"336 885 6670  ",
+    //     email:"hello@architronix.com",
+    //     lat_lng:[51.5132388, -0.0080891]
+    // },
 
 ]
