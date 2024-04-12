2976e8ab802129e47c2325e72168339cbf944812
vipulharsh <harshvipul.1704@gmail.com>
2024-04-13 01:35:19 +0530

    made changes

--- src/components/header/mobileNavbar.jsx
+++ src/components/header/mobileNavbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import Logo from "@/assets/icons/logo";
+
 import MenuIcon from "@/assets/icons/menuIcon";
 import { IoMdClose } from "react-icons/io";
 import { menuList } from "@/lib/fackData/menuList";
